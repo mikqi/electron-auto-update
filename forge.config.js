5b182e7f6599fc68a357b873af93630722278d6e
Muhammad Rivki <muhammad.rivki@live.com>
2022-06-29 19:10:54 +0700

    feat: update forge config

--- forge.config.js
+++ forge.config.js
@@ -1,4 +1,41 @@
 module.exports = {
+  "packagerConfig": {},
+  "makers": [
+    {
+      "name": "@electron-forge/maker-squirrel",
+      "config": {
+        "name": "electron_auto_update"
+      }
+    },
+    {
+      "name": "@electron-forge/maker-zip",
+      "platforms": [
+        "darwin"
+      ]
+    },
+    {
+      "name": "@electron-forge/maker-deb",
+      "config": {}
+    }
+  ],
+  "plugins": [
+    [
+      "@electron-forge/plugin-webpack",
+      {
+        "mainConfig": "./webpack.main.config.js",
+        "renderer": {
+          "config": "./webpack.renderer.config.js",
+          "entryPoints": [
+            {
+              "html": "./src/index.html",
+              "js": "./src/renderer.ts",
+              "name": "main_window"
+            }
+          ]
+        }
+      }
+    ]
+  ],
   publishers: [
     {
       name: '@electron-forge/publisher-github',
